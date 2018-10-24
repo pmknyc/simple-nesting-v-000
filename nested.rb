00de56f0121cc4d38d64e71aee877567cbf0a877
Paula McKinley <pmknyc@optimum.net>
2018-10-24 18:18:56 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -1,7 +1,22 @@
 require 'pry'
 def hopper
+	programmer_hash =
+		{
+				:grace_hopper => {
+					:known_for => "COBOL",
+					:languages => ["COBOL", "FORTRAN"]
+				},
+				:alan_kay => {
+					:known_for => "Object Orientation",
+					:languages => ["Smalltalk", "LISP"]
+				},
+				:dennis_ritchie => {
+					:known_for => "Unix",
+					:languages => ["C"]
+				}
+		 }
 binding.pry
-	programmer_hash.collect[:grace_hopper]
+	programmer_hash[:grace_hopper]
 binding.pry
 end
 
