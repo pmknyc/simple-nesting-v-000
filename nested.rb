9b51ef2cd34b029b4c35218920394a200def21df
Paula McKinley <pmknyc@optimum.net>
2018-10-24 18:05:53 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -1,22 +1,8 @@
-def hopper
-	programmer_hash =
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
+def hopper(programmer_hash)
+	programmer_hash[:grace_hopper]
 end
 
-def alan_kay_is_known_for
+def alan_kay_is_known_for(programmer_hash)
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	programmer_hash[:alan_kay][:known_for]
 end
